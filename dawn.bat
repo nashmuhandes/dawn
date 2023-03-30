55bf24a54963e89bf3c611b6dd4ddf57e4af9d00
nashmuhandes <nashbackslash@gmail.com>
2023-03-31 07:54:09 +0800

    - Automate the conversion of gfx LMPs - Add the console background gfx

--- dawn.bat
+++ dawn.bat
@@ -4,16 +4,29 @@ echo.
 
 set dontrun=%1
 
+REM Convert all TGAs to GFX LMPs
+cd src_gfx
+for %%v in (*.tga) do (
+	tga2lmp %%v
+	if exist %%~nv.lmp xcopy %%~nv.lmp ..\gfx\%%~nv.lmp* /Y
+	if exist %%~nv.lmp del %%~nv.lmp
+)
+cd ..
+
+REM delete old progs
 if exist progs.dat del progs.dat
 if exist progs.lno del progs.lno
 
+REM Compile QuakeC
 cd src_qc
 fteqcc64 -src %cd%
 
+REM Cleanup
 cd ..
 if not exist progs.dat goto error
 del progs.lno
 
+REM Don't run the game if specified
 if defined dontrun goto done
 
 cd ..
